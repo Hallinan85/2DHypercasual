51bc7f22b8c14100701244ba0dfb7ba7262eab10
Hallinan85 <dan.hallinan@outlook.com>
2021-10-13 19:18:38 +0100

    New Rocket added

---
+++ 2D Hypercasual (1)/Assets/Scripts/Score2nd.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score2nd : MonoBehaviour
+{
+    public Text scoreText;
+    public float scoreAmount;
+    public float pointIncreasedPerSecond;
+    void Start()
+    {
+        scoreAmount = 0f;
+        pointIncreasedPerSecond = 1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = (int)scoreAmount + " Score";
+        scoreAmount += pointIncreasedPerSecond * Time.deltaTime;
+    }
+}
